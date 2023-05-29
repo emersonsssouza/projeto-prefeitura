b92da05ebbc032cc442986202e69807870acdc70
emerson souza <emerson-santos1940@hotmail.com>
2023-05-28 22:25:14 -0300

    atualização de projeto
    divs e classes da area de noticias estão sendo refeitas

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,6 @@
     let agora = new Date()
     let dia = agora.getDate();
-    let mes = agora.getMonth();
-    let mesatual = agora.getUTCMonth();
+    let mes = agora.getMonth() +1;
     let ano = agora.getFullYear();
     console.log(`${dia} de ${mes} de ${ano}`);
 
