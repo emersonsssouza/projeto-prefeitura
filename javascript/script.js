f71b0674911a3fbc2c35a0ea837464036db83d27
emerson souza <emerson-santos1940@hotmail.com>
2023-09-30 00:00:42 -0300

    menu sidebar
    menu Sidebar

--- javascript/script.js
+++ javascript/script.js
@@ -9,4 +9,36 @@
     let seletordiv = document.getElementById('res').style.background = 'white';
     let mostrares = document.getElementById('res').innerHTML = resultado;
 
+    function mudouTamanho() {
+        if (window.innerWidth >= 768) {
+            itens.style.display = 'block'
+        }
+
+        else{
+            itens.style.display  = 'none'
+        }
+    }
+
+    function clickMenu() {
+     
+        if (itens.style.display == 'block') {
+            itens.style.display = 'none'
+        } 
+
+        else {
+            itens.style.display = 'block'
+        }
+    }
+
+    function clickSidebar() {
+     
+        if (sidebar.style.display == 'block') {
+            sidebar.style.display = 'none'
+        } 
+
+        else {
+            sidebar.style.display = 'block'
+        }
+    }
+
     
